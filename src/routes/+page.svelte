62416d232d48db10bec19f69f17bc111d8b47033
Vladimir <saurinvladimir5@gmail.com>
2024-04-03 15:30:49 +0300

    add main page and nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,28 @@
 </svelte:head>
 
 <div class='container'>
-<div class='info'>
-<div class='animate-background'>
-<div class='container info-container'>
-<div class='info-text'>
-<h1>
-<p class='animate-text-1'>Lorem ipsum dolor sir,</p>
-<p class='text-2'>Lorem ipsum dolor sir amet</p>
-</h1>
+  <div class='info'>
+    <div class='animate-background'>
+      <div class='container info-container'>
+        <div class='info-text'>
+          <h1>
+            <p class='animate-text-1'>Lorem ipsum dolor sir,</p>
+            <p class='text-2'>Lorem ipsum dolor sir amet</p>
+          </h1>
+        </div>
+        <h2 class='subtitle'>
+             Lorem ipsum dolor sir amet, consectetur adipisicing elit. Accusantium architecto aut, cupiditate dolores
+          ducimus expedita labore nemo odio quidusdam quidem sapiente soluta.
+        </h2>
+        <div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
+          <a href='/about'>Click Here</a>
+          <div class='arrow'> --> 
+       </div>
+     </div>
+   </div>
+ </div>
 </div>
-<h2 class='subtitle'>
-Lorem ipsum dolor sir amet, consectetur adipisicing elit. Accusantium architecto aut, cupiditate dolores
-ducimus expedita labore nemo odio quidusdam quidem sapiente soluta.
-</h2>
-<div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
-<a href='/about'>Click Here</a>
-<div class='arrow'> --> 
-</div>
-</div>
-</div>
-</div>
-</div>
+
 <div class='background-div'></div>
 
 <style> 
