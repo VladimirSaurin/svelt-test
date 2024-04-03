f768de08784ce2c689fdfbc572a710693ab0b6d3
Vladimir <saurinvladimir5@gmail.com>
2024-04-03 16:45:39 +0300

    add pages

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Блог</h1>
+<svelte:head>
+
+ <title>Блог</title>
+</svelte:head>
+<div class='container'
+ <h1>Мои статьи</h1>
+ <div class='blogposts'>
+  {#each routes as page}
+   <div class='post'>
+    <h2>{page.title}</h2>
+    <p>{page.body}</p>
+    <p class='readmore'>
+     <a class='link' href={'/blog/posts/${page.id}'}>
+      Читать далее
+     </a>
+    </p>
+   </div>
+  {/each}
+ </div>
+</div>
